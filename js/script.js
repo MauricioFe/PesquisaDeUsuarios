a2a0eba1c68e0d884859b9261a2f1ac90cf0b5d8
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-07-24 14:44:21 -0300

    Refactor code

--- js/script.js
+++ js/script.js
@@ -18,10 +18,9 @@ window.addEventListener('load', () => {
 
 
 async function fecthUsers() {
-    //const response = await fetch('https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo');
-    //const data = await response.json();
-
-    usersList = people.results.map(user => {
+    const response = await fetch('https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo');
+    const data = await response.json();
+    usersList = data.results.map(user => {
         const { name, picture, dob, gender } = user;
         return {
             name: name.first + " " + name.last,
@@ -29,7 +28,16 @@ async function fecthUsers() {
             dob: dob.age,
             gender: gender
         }
-    })
+    });
+    // usersList = people.results.map(user => {
+    //     const { name, picture, dob, gender } = user;
+    //     return {
+    //         name: name.first + " " + name.last,
+    //         picture: picture.medium,
+    //         dob: dob.age,
+    //         gender: gender
+    //     }
+    // })
 }
 
 function initializeComponents() {
